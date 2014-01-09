df43b62778d757aee511b85d02e7099d4e90bf04
hyunrails <hyun1@swarthmore.edu>
2014-01-09 06:36:45 -0500

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  #let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Swarthmore Course Evaluation' }
+    let(:page_title) { '' }
 
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
@@ -24,4 +33,34 @@ describe "Static pages" do
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
   end
+
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+  end
+
+=begin
+  # Test for rendering the feed on the Home page.
+  describe "for signed-in users" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        # Note that the first # in li##{item.id} is Capybara syntax
+        # for a CSS id, whereas the second # is the beginning of
+        # a Ruby string interpolation #{}.
+        expect(page).to have_selector("li##{item.id}", text: item.content)
+      end
+    end
+  end
+=end
+
 end

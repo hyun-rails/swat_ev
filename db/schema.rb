e7ebf8c9cb5adfa1e870f5cf24f39b960c8bbd8d
hyunrails <hyun1@swarthmore.edu>
2014-01-10 05:51:18 -0500

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109113203) do
+ActiveRecord::Schema.define(version: 20140110104143) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest", limit: nil
+    t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end

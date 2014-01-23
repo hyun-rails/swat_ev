8661f30f4f828d57cc3ec8d6f3dc9d7bc844967c
hyunrails <hyun1@swarthmore.edu>
2014-01-22 21:26:42 -0500

    change post content limit to 5000

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
-	validates :content, presence: true, length: { maximum: 500 }
+	validates :content, presence: true, length: { maximum: 5000 }
 	validates :user_id, presence: true
     validates :professor, presence: true
 	DEPARTMENT = [ ["ECON", 0], ["CPSC", 1] ]
